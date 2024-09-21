830354c1f1fc896747f6ec591dd4a65cb584c829
Jaymart <jaymart.b.araga@gmail.com>
2024-09-21 19:35:35 +0800

    First lesson

--- app/src/main/java/ph/mart/todo/MainActivity.kt
+++ app/src/main/java/ph/mart/todo/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ph.mart.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TodoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,10 +42,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainApp(
+    modifier: Modifier = Modifier
+) {
+    var userInput by remember { mutableStateOf("") }
+    var isDialogShown by remember { mutableStateOf(false) }
+
+    if (isDialogShown) {
+        MyCustomDialog(
+            onDismissRequest = { isDialogShown = false },
+            onConfirm = { isDialogShown = false },
+            text = userInput
+        )
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+    ) {
+        Text("Hello!")
+
+        TextField(
+            value = userInput,
+            onValueChange = { userInput = it },
+            label = { Text("Enter something") }
+        )
+
+        Button(
+            onClick = { isDialogShown = true }
+        ) {
+            Text("Click Me")
+        }
+    }
+}
+
+@Composable
+fun MyCustomDialog(
+    onDismissRequest: () -> Unit,
+    onConfirm: () -> Unit,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text("Confirm")
+            }
+        },
+        text = {
+            Text("Input: $text")
+        }
     )
 }
 
@@ -42,6 +102,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TodoTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            MainApp(
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
     }
 }
